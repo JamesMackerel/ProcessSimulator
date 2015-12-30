1a205c83c4645f36a48360f6ed57a488cd6fe5b6
JamesMackerel <ghost.mackerel@outlook.com>
2015-12-31 02:33:19 +0800

    add ISimulator; add VirtualProcess;

---
+++ ProcessSimulator/VirtualProcess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessSimulator
+{
+    class VirtualProcess
+    {
+        #region user control attribute
+        public string Pid {set; get; }
+        public int Priority { set; get; }
+        public DateTime ArriveTime { set; get; }
+        public TimeSpan Duration { set; get; }
+        #endregion
+
+        #region non user control attribute
+        public DateTime StartTime { set; get; }
+        public DateTime CompleteTime { set; get; }
+        #endregion
+    }
+}
